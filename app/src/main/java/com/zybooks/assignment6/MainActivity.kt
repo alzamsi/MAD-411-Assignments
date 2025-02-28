bc5d2367ee99f25c0709671cd31e34adc4cc6ea0
Sultan Al Shamsi <w0856089@myscc.ca>
2025-02-28 12:32:32 -0500

    Assignment6

---
+++ app/src/main/java/com/zybooks/assignment6/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.zybooks.assignment6
+
+import android.os.Bundle
+import android.widget.AutoCompleteTextView.Validator
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val edName = findViewById<EditText>(R.id.editName)
+        val btuShowName = findViewById<Button>(R.id.button)
+        val result = findViewById<TextView>(R.id.showNameTextView)
+
+        btuShowName.setOnClickListener{
+            val name = edName.text.toString()
+            if (name.isNotEmpty()){
+                result.text = "Hello: $name"
+            }else{
+                result.text = "Please Enter Your Name"
+            }
+        }
+    }
+}
